62155bda1b8c9b2d20df837fb35fcdd4750b8210
douira <douira100@gmail.com>
2025-01-12 21:55:58 +0100

    refactor old translucent data reuse to be a method with implementations instead of using instanceof
    Signed-off-by: douira <douira100@gmail.com>

--- common/src/main/java/net/caffeinemc/mods/sodium/client/render/chunk/translucent_sorting/data/PresentTranslucentData.java
+++ common/src/main/java/net/caffeinemc/mods/sodium/client/render/chunk/translucent_sorting/data/PresentTranslucentData.java
@@ -1,5 +1,8 @@
 package net.caffeinemc.mods.sodium.client.render.chunk.translucent_sorting.data;
 
+import net.caffeinemc.mods.sodium.client.render.chunk.translucent_sorting.SortType;
+import net.caffeinemc.mods.sodium.client.render.chunk.translucent_sorting.TQuad;
+import net.caffeinemc.mods.sodium.client.render.chunk.translucent_sorting.TranslucentGeometryCollector;
 import net.minecraft.core.SectionPos;
 
 /**
@@ -17,6 +20,12 @@ public abstract class PresentTranslucentData extends TranslucentData {
     public abstract int[] getVertexCounts();
 
     public abstract Sorter getSorter();
+
+    @Override
+    public boolean oldDataMatches(TranslucentGeometryCollector collector, SortType sortType, TQuad[] quads, int[] vertexCounts) {
+        // for the sort types other than NONE (and the old data being AnyOrderData) the geometry needs to be the same (checked with length and hash)
+        return this.getQuadCount() == quads.length && this.quadHash == collector.getQuadHash(quads);
+    }
 
     public void setQuadHash(int hash) {
         this.quadHash = hash;
