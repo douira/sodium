214b0ad01d29b5f16dbf5e97d10e233969ec9689
JellySquid <jellysquid@pm.me>
2025-01-25 12:42:23 -0600

    Only hook RenderSystem#recordRenderCall in debug mode
    The other assertions are not necessary in newer versions,
    since they all check the thread now.

--- common/src/main/java/net/caffeinemc/mods/sodium/mixin/debug/checks/threading/MixinRenderSystem.java
+++ common/src/main/java/net/caffeinemc/mods/sodium/mixin/debug/checks/threading/MixinRenderSystem.java
@@ -3,12 +3,10 @@ package net.caffeinemc.mods.sodium.mixin.debug.checks.threading;
 import com.mojang.blaze3d.pipeline.RenderCall;
 import com.mojang.blaze3d.systems.RenderSystem;
 import net.caffeinemc.mods.sodium.client.render.util.DeferredRenderTask;
-import net.caffeinemc.mods.sodium.client.render.util.RenderAsserts;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Overwrite;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(RenderSystem.class)
@@ -19,31 +17,12 @@ public abstract class MixinRenderSystem {
      */
     @Overwrite(remap = false)
     public static void recordRenderCall(RenderCall call) {
-        throw new UnsupportedOperationException("RenderSystem#recordRenderCall is not supported when using Sodium");
+        throw new UnsupportedOperationException("Usage of RenderSystem#recordRenderCall is likely a bug, " +
+                "which is handled as an error when Sodium is enabled in debug mode");
     }
 
-    @Inject(method = "replayQueue", at = @At("HEAD"), remap = false)
-    private static void onReplayQueue(CallbackInfo ci) {
+    @Inject(method = "replayQueue", at = @At("HEAD"))
+    private static void handleDeferredTasks(CallbackInfo ci) {
         DeferredRenderTask.runAll();
-    }
-
-    @Redirect(method = {
-            "setShaderColor",
-            "lineWidth",
-            "glGenBuffers",
-            "glGenVertexArrays",
-            "setShader",
-            "setShaderTexture(ILnet/minecraft/resources/ResourceLocation;)V",
-            "setShaderTexture(II)V",
-            "setProjectionMatrix",
-            "setTextureMatrix",
-            "resetTextureMatrix",
-            "applyModelViewMatrix",
-            "backupProjectionMatrix",
-            "restoreProjectionMatrix",
-            "setShaderGameTime"
-    }, at = @At(value = "INVOKE", target = "Lcom/mojang/blaze3d/systems/RenderSystem;isOnRenderThread()Z"), remap = false)
-    private static boolean validateCurrentThread$glCommands() {
-        return RenderAsserts.validateCurrentThread();
     }
 }
