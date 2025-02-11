415fc85532def7df07879f6805e13db916b6adc5
JellySquid <jellysquid@pm.me>
2025-02-10 21:28:35 -0600

    Bump version to 0.7.0

--- buildSrc/src/main/kotlin/BuildConfig.kt
+++ buildSrc/src/main/kotlin/BuildConfig.kt
@@ -10,7 +10,7 @@ object BuildConfig {
     val PARCHMENT_VERSION: String? = "2024.12.07"
 
     // https://semver.org/
-    var MOD_VERSION: String = "0.6.7-rc.1"
+    var MOD_VERSION: String = "0.7.0"
 
     fun createVersionString(project: Project): String {
         val builder = StringBuilder()
