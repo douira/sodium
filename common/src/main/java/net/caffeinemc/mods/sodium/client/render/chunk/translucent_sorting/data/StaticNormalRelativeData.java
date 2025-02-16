douira/sodium

commit d74e4217fdbc3ad584f58f55d080d10ef3dc4f19

Improve vertex sorting code and use radix sorts
For larger arrays of floats (>80 elements), the radix sort
is significantly faster.

We also merge the process of calculating each quad's centroid
into the metric calculation, so that we can avoid expensive
object de-references, and so that it isn't necessary to
allocate additional memory prior to sorting.

common/src/main/java/net/caffeinemc/mods/sodium/client/render/chunk/translucent_sorting/data/StaticNormalRelativeData.java
@@ -44,31 +44,21 @@ public class StaticNormalRelativeData extends SplitDirectionData {
         var indexBuffer = sorter.getIntBuffer();
 
         if (quads.length <= 1) {
+            // Avoid allocations when there is nothing to sort.
             TranslucentData.writeQuadVertexIndexes(indexBuffer, 0);
-        } else if (RadixSort.useRadixSort(quads.length)) {
+        } else {
             final var keys = new int[quads.length];
+            final var perm = new int[quads.length];
 
             for (int q = 0; q < quads.length; q++) {
                 keys[q] = MathUtil.floatToComparableInt(quads[q].getAccurateDotProduct());
+                perm[q] = q;
             }
 
-            var indices = RadixSort.sort(keys);
+            RadixSort.sortIndirect(perm, keys);
 
             for (int i = 0; i < quads.length; i++) {
-                TranslucentData.writeQuadVertexIndexes(indexBuffer, indices[i]);
-            }
-        } else {
-            final var sortData = new long[quads.length];
-
-            for (int q = 0; q < quads.length; q++) {
-                int dotProductComponent = MathUtil.floatToComparableInt(quads[q].getAccurateDotProduct());
-                sortData[q] = (long) dotProductComponent << 32 | q;
-            }
-
-            Arrays.sort(sortData);
-
-            for (int i = 0; i < quads.length; i++) {
-                TranslucentData.writeQuadVertexIndexes(indexBuffer, (int) sortData[i]);
+                TranslucentData.writeQuadVertexIndexes(indexBuffer, perm[i]);
             }
         }
 
@@ -86,21 +76,14 @@ public class StaticNormalRelativeData extends SplitDirectionData {
         var indexBuffer = sorter.getIntBuffer();
 
         var maxQuadCount = 0;
-        boolean anyNeedsSortData = false;
+
         for (var vertexCount : vertexCounts) {
             if (vertexCount != -1) {
                 var quadCount = TranslucentData.vertexCountToQuadCount(vertexCount);
                 maxQuadCount = Math.max(maxQuadCount, quadCount);
-                anyNeedsSortData |= !RadixSort.useRadixSort(quadCount) && quadCount > 1;
             }
         }
 
-        long[] sortData = null;
-        if (anyNeedsSortData) {
-            sortData = new long[maxQuadCount];
-        }
-
-        int quadIndex = 0;
         for (var vertexCount : vertexCounts) {
             if (vertexCount == -1 || vertexCount == 0) {
                 continue;
@@ -110,32 +93,19 @@ public class StaticNormalRelativeData extends SplitDirectionData {
 
             if (count == 1) {
                 TranslucentData.writeQuadVertexIndexes(indexBuffer, 0);
-                quadIndex++;
-            } else if (RadixSort.useRadixSort(count)) {
-                final var keys = new int[count];
-
-                for (int q = 0; q < count; q++) {
-                    keys[q] = MathUtil.floatToComparableInt(quads[quadIndex++].getAccurateDotProduct());
-                }
-
-                var indices = RadixSort.sort(keys);
-
-                for (int i = 0; i < count; i++) {
-                    TranslucentData.writeQuadVertexIndexes(indexBuffer, indices[i]);
-                }
             } else {
-                for (int i = 0; i < count; i++) {
-                    var quad = quads[quadIndex++];
-                    int dotProductComponent = MathUtil.floatToComparableInt(quad.getAccurateDotProduct());
-                    sortData[i] = (long) dotProductComponent << 32 | i;
+                final var keys = new int[count];
+                final var perm = new int[count];
+
+                for (int idx = 0; idx < count; idx++) {
+                    keys[idx] = MathUtil.floatToComparableInt(quads[idx].getAccurateDotProduct());
+                    perm[idx] = idx;
                 }
 
-                if (count > 1) {
-                    Arrays.sort(sortData, 0, count);
-                }
+                RadixSort.sortIndirect(perm, keys);
 
-                for (int i = 0; i < count; i++) {
-                    TranslucentData.writeQuadVertexIndexes(indexBuffer, (int) sortData[i]);
+                for (int idx = 0; idx < count; idx++) {
+                    TranslucentData.writeQuadVertexIndexes(indexBuffer, perm[idx]);
                 }
             }
         }

common/src/main/java/net/caffeinemc/mods/sodium/client/render/chunk/translucent_sorting/bsp_tree/InnerPartitionBSPNode.java
@@ -530,43 +530,25 @@ abstract class InnerPartitionBSPNode extends BSPNode {
     }
 
     static private BSPNode buildSNRLeafNodeFromQuads(BSPWorkspace workspace, IntArrayList indexes, LongArrayList points) {
-        // in this case the points array is wrong, but its allocation can be reused
+        final var indexBuffer = indexes.elements();
+        final var indexCount = indexes.size();
 
-        int[] quadIndexes;
+        final var keys = new int[indexCount];
+        final var perm = new int[indexCount];
 
-        // adapted from SNR sorting code
-        if (RadixSort.useRadixSort(indexes.size())) {
-            final var keys = new int[indexes.size()];
-
-            for (int i = 0; i < indexes.size(); i++) {
-                var quadIndex = indexes.getInt(i);
-                keys[i] = MathUtil.floatToComparableInt(workspace.quads[quadIndex].getAccurateDotProduct());
-            }
-
-            quadIndexes = RadixSort.sort(keys);
-
-            for (int i = 0; i < indexes.size(); i++) {
-                quadIndexes[i] = indexes.getInt(quadIndexes[i]);
-            }
-        } else {
-            final var sortData = points.elements();
-
-            for (int i = 0; i < indexes.size(); i++) {
-                var quadIndex = indexes.getInt(i);
-                int dotProductComponent = MathUtil.floatToComparableInt(workspace.quads[quadIndex].getAccurateDotProduct());
-                sortData[i] = (long) dotProductComponent << 32 | quadIndex;
-            }
-
-            Arrays.sort(sortData, 0, indexes.size());
-
-            quadIndexes = new int[indexes.size()];
-
-            for (int i = 0; i < indexes.size(); i++) {
-                quadIndexes[i] = (int) sortData[i];
-            }
+        for (int i = 0; i < indexCount; i++) {
+            TQuad quad = workspace.quads[indexBuffer[i]];
+            keys[i] = MathUtil.floatToComparableInt(quad.getAccurateDotProduct());
+            perm[i] = i;
         }
 
-        return new LeafMultiBSPNode(BSPSortState.compressIndexes(IntArrayList.wrap(quadIndexes), false));
+        RadixSort.sortIndirect(perm, keys);
+
+        for (int i = 0; i < indexCount; i++) {
+            perm[i] = indexBuffer[perm[i]];
+        }
+
+        return new LeafMultiBSPNode(BSPSortState.compressIndexes(IntArrayList.wrap(perm), false));
     }
 
     static private BSPNode buildSNRLeafNodeFromPoints(BSPWorkspace workspace, LongArrayList points) {

common/src/main/java/net/caffeinemc/mods/sodium/client/render/chunk/translucent_sorting/data/DynamicTopoData.java
@@ -9,7 +9,6 @@ import org.joml.Vector3dc;
 import org.joml.Vector3fc;
 
 import java.nio.IntBuffer;
-import java.util.Arrays;
 import java.util.function.IntConsumer;
 
 /**
@@ -221,30 +220,22 @@ public class DynamicTopoData extends DynamicData {
      */
     static void distanceSortDirect(IntBuffer indexBuffer, TQuad[] quads, Vector3fc cameraPos) {
         if (quads.length <= 1) {
+            // Avoid allocations when there is nothing to sort.
             TranslucentData.writeQuadVertexIndexes(indexBuffer, 0);
-        } else if (RadixSort.useRadixSort(quads.length)) {
-            final var keys = new int[quads.length];
-
-            for (int q = 0; q < quads.length; q++) {
-                keys[q] = ~Float.floatToRawIntBits(quads[q].getCenter().distanceSquared(cameraPos));
-            }
-
-            var indices = RadixSort.sort(keys);
-
-            for (int i = 0; i < quads.length; i++) {
-                TranslucentData.writeQuadVertexIndexes(indexBuffer, indices[i]);
-            }
         } else {
-            final var data = new long[quads.length];
-            for (int q = 0; q < quads.length; q++) {
-                float distance = quads[q].getCenter().distanceSquared(cameraPos);
-                data[q] = (long) ~Float.floatToRawIntBits(distance) << 32 | q;
+            final var keys = new int[quads.length];
+            final var perm = new int[quads.length];
+
+            for (int idx = 0; idx < quads.length; idx++) {
+                var centroid = quads[idx].getCenter();
+                keys[idx] = ~Float.floatToRawIntBits(centroid.distanceSquared(cameraPos));
+                perm[idx] = idx;
             }
 
-            Arrays.sort(data);
+            RadixSort.sortIndirect(perm, keys);
 
-            for (int i = 0; i < quads.length; i++) {
-                TranslucentData.writeQuadVertexIndexes(indexBuffer, (int) data[i]);
+            for (int idx = 0; idx < quads.length; idx++) {
+                TranslucentData.writeQuadVertexIndexes(indexBuffer, perm[idx]);
             }
         }
     }

common/src/main/java/net/caffeinemc/mods/sodium/client/util/sorting/AbstractSort.java
@@ -1,14 +1,0 @@
-package net.caffeinemc.mods.sodium.client.util.sorting;
-
-
-public class AbstractSort {
-    protected static int[] createIndexBuffer(int length) {
-        var indices = new int[length];
-
-        for (int i = 0; i < length; i++) {
-            indices[i] = i;
-        }
-
-        return indices;
-    }
-}

common/src/main/java/net/caffeinemc/mods/sodium/client/util/sorting/InsertionSort.java
@@ -1,27 +1,0 @@
-package net.caffeinemc.mods.sodium.client.util.sorting;
-
-public class InsertionSort extends AbstractSort {
-    public static void insertionSort(final int[] indices, final int fromIndex, final int toIndex, final float[] keys) {
-        int index = fromIndex;
-
-        while (++index < toIndex) {
-            int t = indices[index];
-            int j = index;
-
-            int u = indices[j - 1];
-
-            while (keys[u] < keys[t]) {
-                indices[j] = u;
-
-                if (fromIndex == j - 1) {
-                    --j;
-                    break;
-                }
-
-                u = indices[--j - 1];
-            }
-
-            indices[j] = t;
-        }
-    }
-}

common/src/main/java/net/caffeinemc/mods/sodium/client/util/sorting/MergeSort.java
@@ -1,89 +1,0 @@
-/*
- * Copyright (C) 2002-2017 Sebastiano Vigna
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- *
- *
- *
- * For the sorting and binary search code:
- *
- * Copyright (C) 1999 CERN - European Organization for Nuclear Research.
- *
- *   Permission to use, copy, modify, distribute and sell this software and
- *   its documentation for any purpose is hereby granted without fee,
- *   provided that the above copyright notice appear in all copies and that
- *   both that copyright notice and this permission notice appear in
- *   supporting documentation. CERN makes no representations about the
- *   suitability of this software for any purpose. It is provided "as is"
- *   without expressed or implied warranty.
- */
-package net.caffeinemc.mods.sodium.client.util.sorting;
-
-
-/**
- * Based upon {@link it.unimi.dsi.fastutil.ints.IntArrays} implementation, but it eliminates the use of a user-supplied
- * function and instead sorts an array of floats directly. This helps to improve runtime performance.
- */
-public class MergeSort extends AbstractSort {
-    private static final int INSERTION_SORT_THRESHOLD = 16;
-
-    public static int[] mergeSort(float[] keys) {
-        var indices = createIndexBuffer(keys.length);
-        mergeSort(indices, keys);
-
-        return indices;
-    }
-
-    private static void mergeSort(final int[] indices, final float[] keys) {
-        mergeSort(indices, keys, 0, indices.length, null);
-    }
-
-    private static void mergeSort(final int[] indices, final float[] keys, final int fromIndex, final int toIndex, int[] supp) {
-        int len = toIndex - fromIndex;
-
-        // Insertion sort on smallest arrays
-        if (len < INSERTION_SORT_THRESHOLD) {
-            InsertionSort.insertionSort(indices, fromIndex, toIndex, keys);
-            return;
-        }
-
-        if (supp == null) {
-            supp = indices.clone();
-        }
-
-        // Recursively sort halves of a into supp
-        final int mid = (fromIndex + toIndex) >>> 1;
-        mergeSort(supp, keys, fromIndex, mid, indices);
-        mergeSort(supp, keys, mid, toIndex, indices);
-
-        // If list is already sorted, just copy from supp to indices. This is an
-        // optimization that results in faster sorts for nearly ordered lists.
-        if (keys[supp[mid]] <= keys[supp[mid - 1]]) {
-            System.arraycopy(supp, fromIndex, indices, fromIndex, len);
-            return;
-        }
-
-        // Merge sorted halves (now in supp) into indices
-        int i = fromIndex, p = fromIndex, q = mid;
-
-        while (i < toIndex) {
-            if (q >= toIndex || p < mid && keys[supp[q]] <= keys[supp[p]]) {
-                indices[i] = supp[p++];
-            } else {
-                indices[i] = supp[q++];
-            }
-
-            i++;
-        }
-    }
-}

common/src/main/java/net/caffeinemc/mods/sodium/client/util/sorting/RadixSort.java
@@ -1,7 +1,9 @@
 package net.caffeinemc.mods.sodium.client.util.sorting;
 
-public class RadixSort extends AbstractSort {
-    public static final int RADIX_SORT_THRESHOLD = 64;
+import it.unimi.dsi.fastutil.ints.IntArrays;
+
+public class RadixSort {
+    private static final int RADIX_SORT_THRESHOLD = 80;
 
     private static final int DIGIT_BITS = 8;
     private static final int RADIX_KEY_BITS = Integer.BYTES * 8;
@@ -9,27 +11,15 @@ public class RadixSort extends AbstractSort {
     private static final int DIGIT_COUNT = (RADIX_KEY_BITS + DIGIT_BITS - 1) / DIGIT_BITS;
     private static final int DIGIT_MASK = (1 << DIGIT_BITS) - 1;
 
-    public static int[] sort(int[] keys) {
-        if (keys.length <= 1) {
-            return new int[keys.length];
-        }
-
-        return radixSort(keys, createHistogram(keys));
-    }
-
-    private static int[][] createHistogram(int[] keys) {
-        var histogram = new int[DIGIT_COUNT][BUCKET_COUNT];
-
+    private static void getHistogram(int[][] histogram, int[] keys) {
         for (final int key : keys) {
             for (int digit = 0; digit < DIGIT_COUNT; digit++) {
                 histogram[digit][extractDigit(key, digit)] += 1;
             }
         }
-
-        return histogram;
     }
 
-    private static void prefixSum(int[][] offsets) {
+    private static void prefixSums(int[][] offsets) {
         for (int digit = 0; digit < DIGIT_COUNT; digit++) {
             final var buckets = offsets[digit];
             var sum = 0;
@@ -42,13 +32,55 @@ public class RadixSort extends AbstractSort {
         }
     }
 
-    private static int[] radixSort(int[] keys, int[][] offsets) {
-        prefixSum(offsets);
+    /**
+     * <p>Sorts the specified array according to the natural ascending order using an unstable, out-of-place, indirect,
+     * 256-way LSD radix sort.</p>
+     *
+     * <p>This algorithm is well suited for large arrays of integers, especially when they are uniformly distributed
+     * over the entire range of the data type.</p>
+     *
+     * <p>This method implements an <em>indirect</em> sort. The elements of {@param perm} (which must be
+     * exactly the numbers in the interval {@code [0..perm.length)}) will be permuted so that
+     * {@code x[perm[i]] < x[perm[i + 1]]}.</p>
+     *
+     * <p>While this radix sort is very fast on larger arrays, there is a certain amount of fixed cost involved in
+     * computing the histogram and prefix sums. Because of this, a fallback algorithm (currently quick sort) is used
+     * for very small arrays to ensure this method performs well for all inputs of all sizes.</p>
+     *
+     * @param perm a permutation array indexing {@param keys}.
+     * @param keys the array of elements to be sorted.
+     */
+    public static void sortIndirect(final int[] perm, final int[] keys) {
+        if (perm.length <= RADIX_SORT_THRESHOLD) {
+            smallSort(perm, keys);
+            return;
+        }
 
-        final var length = keys.length;
+        int[][] offsets;
+        int[] next;
 
-        int[] cur = createIndexBuffer(length);
-        int[] next = new int[length];
+        try {
+            offsets = new int[DIGIT_COUNT][BUCKET_COUNT];
+            next = new int[perm.length];
+        } catch (OutOfMemoryError oom) {
+            // Not enough memory to perform an out-of-place sort, so use an in-place alternative.
+            fallbackInPlaceSort(perm, keys);
+            return;
+        }
+
+        sortIndirect(perm, keys, offsets, next);
+    }
+
+    private static void sortIndirect(final int[] perm,
+                                     final int[] keys,
+                                     final int[][] offsets,
+                                     int[] next)
+    {
+        final int length = perm.length;
+        getHistogram(offsets, keys);
+        prefixSums(offsets);
+
+        int[] cur = perm;
 
         for (int digit = 0; digit < DIGIT_COUNT; digit++) {
             final var buckets = offsets[digit];
@@ -68,15 +100,22 @@ public class RadixSort extends AbstractSort {
                 cur = temp;
             }
         }
+    }
 
-        return cur;
+    private static void smallSort(int[] perm, int[] keys) {
+        if (perm.length <= 1) {
+            return;
+        }
+
+        fallbackInPlaceSort(perm, keys);
+    }
+
+    // Fallback sorting method which is guaranteed to be in-place and not require additional memory.
+    private static void fallbackInPlaceSort(int[] perm, int[] keys) {
+        IntArrays.quickSortIndirect(perm, keys);
     }
 
     private static int extractDigit(int key, int digit) {
         return ((key >>> (digit * DIGIT_BITS)) & DIGIT_MASK);
-    }
-
-    public static boolean useRadixSort(int length) {
-        return length >= RADIX_SORT_THRESHOLD;
     }
 }

common/src/main/java/net/caffeinemc/mods/sodium/client/util/sorting/VertexSorters.java
@@ -1,45 +1,141 @@
 package net.caffeinemc.mods.sodium.client.util.sorting;
 
 import com.mojang.blaze3d.vertex.VertexSorting;
+import net.caffeinemc.mods.sodium.client.util.MathUtil;
+import org.apache.commons.lang3.Validate;
+import org.jetbrains.annotations.NotNull;
 import org.joml.Vector3f;
+import org.lwjgl.system.MemoryUtil;
+
+import java.nio.ByteBuffer;
 
 public class VertexSorters {
-    public static VertexSorting sortByDistance(Vector3f origin) {
-        return new SortByDistance(origin);
+    public static VertexSortingExtended distance(float x, float y, float z) {
+        if (x == 0.0f && y == 0.0f && z == 0.0f) {
+            return SortByDistanceToOrigin.INSTANCE;
+        }
+
+        return new SortByDistanceToPoint(x, y, z);
     }
 
-    private static class SortByDistance extends AbstractVertexSorter {
-        private final Vector3f origin;
-
-        private SortByDistance(Vector3f origin) {
-            this.origin = origin;
-        }
-
-        @Override
-        protected float getKey(Vector3f position) {
-            return this.origin.distanceSquared(position);
-        }
+    public static VertexSortingExtended orthographicZ() {
+        return SortByOrthographicZ.INSTANCE;
     }
 
-    /**
-     * Sorts the keys given by the subclass by descending value.
-     */
-    private static abstract class AbstractVertexSorter implements VertexSorting {
+    // Slow, should only be used when none of the other classes apply.
+    public static VertexSortingExtended fallback(VertexSorting.DistanceFunction metric) {
+        return new SortByFallback(metric);
+    }
+
+    private abstract static class AbstractSorter implements VertexSortingExtended {
         @Override
-        public final int[] sort(Vector3f[] positions) {
-            return this.mergeSort(positions);
-        }
+        public final int @NotNull [] sort(Vector3f[] centroids) {
+            final int length = centroids.length;
+            final var keys = new int[length];
+            final var perm = new int[length];
 
-        private int[] mergeSort(Vector3f[] positions) {
-            final var keys = new float[positions.length];
-
-            for (int index = 0; index < positions.length; index++) {
-                keys[index] = this.getKey(positions[index]);
+            for (int index = 0; index < length; index++) {
+                keys[index] = ~MathUtil.floatToComparableInt(this.applyMetric(centroids[index]));
+                perm[index] = index;
             }
 
-            return MergeSort.mergeSort(keys);
+            RadixSort.sortIndirect(perm, keys);
+
+            return perm;
+        }
+    }
+
+    private static class SortByDistanceToPoint extends AbstractSorter {
+        private final float x, y, z;
+
+        private SortByDistanceToPoint(float x, float y, float z) {
+            this.x = x;
+            this.y = y;
+            this.z = z;
         }
 
-        protected abstract float getKey(Vector3f object);
+        @Override
+        public float applyMetric(float x, float y, float z) {
+            float dx = this.x - x;
+            float dy = this.y - y;
+            float dz = this.z - z;
+
+            return (dx * dx) + (dy * dy) + (dz * dz);
+        }
+    }
+
+    private static class SortByDistanceToOrigin extends AbstractSorter {
+        private static final SortByDistanceToOrigin INSTANCE = new SortByDistanceToOrigin();
+
+        @Override
+        public float applyMetric(float x, float y, float z) {
+            return (x * x) + (y * y) + (z * z);
+        }
+    }
+
+    private static class SortByOrthographicZ extends AbstractSorter {
+        private static final SortByOrthographicZ INSTANCE = new SortByOrthographicZ();
+
+        @Override
+        public float applyMetric(float x, float y, float z) {
+            return -z;
+        }
+    }
+
+    private static class SortByFallback extends AbstractSorter {
+        private final DistanceFunction function;
+        private final Vector3f scratch = new Vector3f();
+
+        private SortByFallback(DistanceFunction function) {
+            this.function = function;
+        }
+
+        @Override
+        public float applyMetric(float x, float y, float z) {
+            return this.function.apply(this.scratch.set(x, y, z));
+        }
+    }
+
+    public static int[] sort(ByteBuffer buffer, int vertexCount, int vertexStride, VertexSortingExtended sorting) {
+        Validate.isTrue(buffer.remaining() >= vertexStride * vertexCount,
+                "Vertex buffer is not large enough to contain all vertices");
+
+        long pVertex0 = MemoryUtil.memAddress(buffer);
+        long pVertex2 = MemoryUtil.memAddress(buffer, vertexStride * 2);
+
+        int primitiveCount = vertexCount / 4;
+        int primitiveStride = vertexStride * 4;
+
+        final int[] keys = new int[primitiveCount];
+        final int[] perm = new int[primitiveCount];
+
+        for (int primitiveId = 0; primitiveId < primitiveCount; primitiveId++) {
+            // Position of vertex[0]
+            float v0x = MemoryUtil.memGetFloat(pVertex0 + 0L);
+            float v0y = MemoryUtil.memGetFloat(pVertex0 + 4L);
+            float v0z = MemoryUtil.memGetFloat(pVertex0 + 8L);
+
+            // Position of vertex[2]
+            float v2x = MemoryUtil.memGetFloat(pVertex2 + 0L);
+            float v2y = MemoryUtil.memGetFloat(pVertex2 + 4L);
+            float v2z = MemoryUtil.memGetFloat(pVertex2 + 8L);
+
+            // The centroid of the quad is calculated using the mid-point of the diagonal edge. This will not work
+            // for degenerate quads, but those are not sortable anyway.
+            float cx = (v0x + v2x) * 0.5F;
+            float cy = (v0y + v2y) * 0.5F;
+            float cz = (v0z + v2z) * 0.5F;
+
+            // The sign bit of the metric is negated as we need back-to-front (descending) ordering.
+            keys[primitiveId] = MathUtil.floatToComparableInt(-sorting.applyMetric(cx, cy, cz));
+            perm[primitiveId] = primitiveId;
+
+            pVertex0 += primitiveStride;
+            pVertex2 += primitiveStride;
+        }
+
+        RadixSort.sortIndirect(perm, keys);
+
+        return perm;
     }
 }

commit b097296d5ca9059d851156fcf87d017188ebe9de

Fix incorrect quad indexing in SNR sorting (#3027)
This changes SNR sorting to use correct (continuous) indexing of the quads
array and adds a comment explaining the tricky behavior.

Resolves the regression introduced with d74e4217fd ("Improve vertex
sorting code and use radix sorts").

common/src/main/java/net/caffeinemc/mods/sodium/client/render/chunk/translucent_sorting/data/StaticNormalRelativeData.java
@@ -6,8 +6,6 @@ import net.caffeinemc.mods.sodium.client.util.MathUtil;
 import net.caffeinemc.mods.sodium.client.util.sorting.RadixSort;
 import net.minecraft.core.SectionPos;
 
-import java.util.Arrays;
-
 /**
  * Static normal relative sorting orders quads by the dot product of their
  * normal and position. (referred to as "distance" throughout the code)
@@ -84,6 +82,10 @@ public class StaticNormalRelativeData extends SplitDirectionData {
             }
         }
 
+        // The quad index is used to keep track of the position in the quad array.
+        // This is necessary because the emitted quad indexes in each facing start at zero,
+        // but the quads are stored in a single continuously indexed array.
+        int quadIndex = 0;
         for (var vertexCount : vertexCounts) {
             if (vertexCount == -1 || vertexCount == 0) {
                 continue;
@@ -93,12 +95,13 @@ public class StaticNormalRelativeData extends SplitDirectionData {
 
             if (count == 1) {
                 TranslucentData.writeQuadVertexIndexes(indexBuffer, 0);
+                quadIndex++;
             } else {
                 final var keys = new int[count];
                 final var perm = new int[count];
 
                 for (int idx = 0; idx < count; idx++) {
-                    keys[idx] = MathUtil.floatToComparableInt(quads[idx].getAccurateDotProduct());
+                    keys[idx] = MathUtil.floatToComparableInt(quads[quadIndex++].getAccurateDotProduct());
                     perm[idx] = idx;
                 }
 