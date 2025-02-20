826722d0b34aa0113df84903e90ef8c47774d47a
douira <douira100@gmail.com>
2025-02-20 20:27:15 +0100

    Implementation of bitmap-based trees for rendering something even when the camera is outside the graph, extracted from decoupled-frustum-test as merge item 2

--- common/src/main/java/net/caffeinemc/mods/sodium/client/render/chunk/RenderSectionFlags.java
+++ common/src/main/java/net/caffeinemc/mods/sodium/client/render/chunk/RenderSectionFlags.java
@@ -5,5 +5,14 @@ public class RenderSectionFlags {
     public static final int HAS_BLOCK_ENTITIES      = 1;
     public static final int HAS_ANIMATED_SPRITES    = 2;
 
+    public static final int MASK_HAS_BLOCK_GEOMETRY      = 1 << HAS_BLOCK_GEOMETRY;
+    public static final int MASK_HAS_BLOCK_ENTITIES      = 1 << HAS_BLOCK_ENTITIES;
+    public static final int MASK_HAS_ANIMATED_SPRITES    = 1 << HAS_ANIMATED_SPRITES;
+    public static final int MASK_NEEDS_RENDER            = MASK_HAS_BLOCK_GEOMETRY | MASK_HAS_BLOCK_ENTITIES | MASK_HAS_ANIMATED_SPRITES;
+
     public static final int NONE = 0;
+
+    public static boolean needsRender(int flags) {
+        return (flags & MASK_NEEDS_RENDER) != 0;
+    }
 }
