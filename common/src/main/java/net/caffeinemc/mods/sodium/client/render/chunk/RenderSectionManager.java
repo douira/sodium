b94ffbf3991023b7cf61d2c1bdf282968d5009a7
douira <douira100@gmail.com>
2025-06-11 00:04:54 +0200

    Fix missing graph dirty when pending section task state changes

--- common/src/main/java/net/caffeinemc/mods/sodium/client/render/chunk/RenderSectionManager.java
+++ common/src/main/java/net/caffeinemc/mods/sodium/client/render/chunk/RenderSectionManager.java
@@ -671,6 +671,9 @@ public class RenderSectionManager {
         }
 
         section.setPendingUpdate(joined, this.lastFrameAtTime);
+        
+        // mark graph as dirty so that it picks up the section's pending task
+        this.markGraphDirty();
 
         return true;
     }
