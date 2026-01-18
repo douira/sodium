ae8726a88fca5c737b3b809a9a6c7f4f5ce0c748
douira <douira100@gmail.com>
2026-01-18 22:57:47 +0100

    extract angle visibility mask calculation into occlusion visitor to account for expanded non-occlusion zone when reusing the result for the whole section or in wide-bfs mode

--- common/src/main/java/net/caffeinemc/mods/sodium/client/render/chunk/occlusion/OcclusionCuller.java
+++ common/src/main/java/net/caffeinemc/mods/sodium/client/render/chunk/occlusion/OcclusionCuller.java
@@ -65,6 +65,30 @@ public class OcclusionCuller {
 
             return planes;
         }
+
+        long UP_DOWN_OCCLUDED = (1L << VisibilityEncoding.bit(GraphDirection.DOWN, GraphDirection.UP)) | (1L << VisibilityEncoding.bit(GraphDirection.UP, GraphDirection.DOWN));
+        long NORTH_SOUTH_OCCLUDED = (1L << VisibilityEncoding.bit(GraphDirection.NORTH, GraphDirection.SOUTH)) | (1L << VisibilityEncoding.bit(GraphDirection.SOUTH, GraphDirection.NORTH));
+        long WEST_EAST_OCCLUDED = (1L << VisibilityEncoding.bit(GraphDirection.WEST, GraphDirection.EAST)) | (1L << VisibilityEncoding.bit(GraphDirection.EAST, GraphDirection.WEST));
+
+        default long getAngleVisibilityMask(Viewport viewport, RenderSection section) {
+            var transform = viewport.getTransform();
+            var dx = Math.abs(transform.x - section.getCenterX());
+            var dy = Math.abs(transform.y - section.getCenterY());
+            var dz = Math.abs(transform.z - section.getCenterZ());
+
+            var angleOcclusionMask = 0L;
+            if (dx > dy || dz > dy) {
+                angleOcclusionMask |= UP_DOWN_OCCLUDED;
+            }
+            if (dx > dz || dy > dz) {
+                angleOcclusionMask |= NORTH_SOUTH_OCCLUDED;
+            }
+            if (dy > dx || dz > dx) {
+                angleOcclusionMask |= WEST_EAST_OCCLUDED;
+            }
+
+            return ~angleOcclusionMask;
+        }
     }
 
     public OcclusionCuller(Long2ReferenceMap<RenderSection> sections, Level level) {
@@ -140,7 +164,7 @@ public class OcclusionCuller {
 
                     // occlude paths through the section if it's being viewed at an angle where
                     // the other side can't possibly be seen
-                    visibilityData &= getAngleVisibilityMask(this.viewport, section);
+                    visibilityData &= this.visitor.getAngleVisibilityMask(this.viewport, section);
 
                     // When using occlusion culling, we can only traverse into neighbors for which there is a path of
                     // visibility through this chunk. This is determined by taking all the incoming paths to this chunk and
@@ -212,30 +236,6 @@ public class OcclusionCuller {
         }
 
         return visibilityData;
-    }
-
-    private static final long UP_DOWN_OCCLUDED = (1L << VisibilityEncoding.bit(GraphDirection.DOWN, GraphDirection.UP)) | (1L << VisibilityEncoding.bit(GraphDirection.UP, GraphDirection.DOWN));
-    private static final long NORTH_SOUTH_OCCLUDED = (1L << VisibilityEncoding.bit(GraphDirection.NORTH, GraphDirection.SOUTH)) | (1L << VisibilityEncoding.bit(GraphDirection.SOUTH, GraphDirection.NORTH));
-    private static final long WEST_EAST_OCCLUDED = (1L << VisibilityEncoding.bit(GraphDirection.WEST, GraphDirection.EAST)) | (1L << VisibilityEncoding.bit(GraphDirection.EAST, GraphDirection.WEST));
-
-    private static long getAngleVisibilityMask(Viewport viewport, RenderSection section) {
-        var transform = viewport.getTransform();
-        var dx = Math.abs(transform.x - section.getCenterX());
-        var dy = Math.abs(transform.y - section.getCenterY());
-        var dz = Math.abs(transform.z - section.getCenterZ());
-
-        var angleOcclusionMask = 0L;
-        if (dx > dy || dz > dy) {
-            angleOcclusionMask |= UP_DOWN_OCCLUDED;
-        }
-        if (dx > dz || dy > dz) {
-            angleOcclusionMask |= NORTH_SOUTH_OCCLUDED;
-        }
-        if (dy > dx || dz > dx) {
-            angleOcclusionMask |= WEST_EAST_OCCLUDED;
-        }
-
-        return ~angleOcclusionMask;
     }
 
     private static boolean isWithinRenderDistance(CameraTransform camera, RenderSection section, float maxDistance) {
