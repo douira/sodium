36c7306f1b07d05a8e0f09443a70a30ddd51223d
douira <douira100@gmail.com>
2026-03-21 18:33:05 +0100

    use out of graph rendering for sync instead of doing a custom graph traversal

--- common/src/main/java/net/caffeinemc/mods/sodium/client/render/SodiumWorldRenderer.java
+++ common/src/main/java/net/caffeinemc/mods/sodium/client/render/SodiumWorldRenderer.java
@@ -229,11 +229,7 @@ public class SodiumWorldRenderer {
 
             profiler.popPush("chunk_render_lists");
 
-            if (updateChunksImmediately) {
-                this.renderSectionManager.renderSync(camera, viewport, fogParameters, spectator);
-            } else {
-                this.renderSectionManager.prepareRenderTrees(camera, viewport, fogParameters, spectator);
-            }
+            this.renderSectionManager.prepareRenderTrees(camera, viewport, fogParameters, spectator);
 
             profiler.popPush("chunk_update");
 
@@ -251,7 +247,7 @@ public class SodiumWorldRenderer {
 
         profiler.popPush("chunk_render_lists");
 
-        this.renderSectionManager.finalizeRenderLists(viewport, fogParameters);
+        this.renderSectionManager.finalizeRenderLists(camera, viewport, fogParameters, updateChunksImmediately);
 
         profiler.popPush("chunk_render_tick");
 
@@ -421,7 +417,7 @@ public class SodiumWorldRenderer {
 
     /**
      * Returns whether the entity intersects with any visible chunks in the graph.
-     *
+     * <p>
      * Note that this method assumes the entity is within the frustum. It does not perform a frustum check.
      *
      * @return True if the entity is visible, otherwise false
